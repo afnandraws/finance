7d8cfaf6488f1c024872ebb9a9472f283b3b1e47
Afnan Khan <40701226+afnandraws@users.noreply.github.com>
2022-10-21 12:50:00 +0100

    conditional styling

--- src/components/Finances/FinanceItem.js
+++ src/components/Finances/FinanceItem.js
@@ -1,8 +1,31 @@
 import styles from "./FinanceItems.module.css";
+let styling = "";
 
 const FinanceItems = (props) => {
+  switch (props.type) {
+    case "Expense":
+      styling = `${styles.card} ${styles.expense}`;
+      break;
+    case "Food":
+      styling = `${styles.card} ${styles.expense}`;
+      break;
+    case "Entertainment":
+      styling = `${styles.card} ${styles.expense}`;
+      break;
+    case "Utilities":
+      styling = `${styles.card} ${styles.expense}`;
+      break;
+
+    default:
+      break;
+  }
+
+  // if (props.type === "Expense") {
+  //   styling = `${styles.card} ${styles.red}`;
+  // }
+
   return (
-    <div className={styles.card}>
+    <div className={styling}>
       <h2>{props.title}</h2>
       <h4> £{props.price}</h4>
     </div>
